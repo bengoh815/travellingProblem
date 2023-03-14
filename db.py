e87f1a64c9d125edf96e79da1a322fbe06284b42
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2023-03-13 23:57:07 -0500

    Update db.py
    figuring out how to maintain data

--- db.py
+++ db.py
@@ -4,11 +4,11 @@ import pandas as pd
 con = sqlite3.connect("mainDB.db")
 cursor = con.cursor()
 # cursor.execute("""SHOW TABLE""")
-cursor.execute("""CREATE TABLE IF NOT EXISTS users(
-    name text,
-    cell_number text,
-    id int
-)""")
+# cursor.execute("""CREATE TABLE IF NOT EXISTS users(
+#     name text,
+#     cell_number text,
+#     id int
+# )""")
 
 # cursor.execute("""CREATE TABLE IF NOT EXISTS geo(
 #     id int,
@@ -17,9 +17,9 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS users(
 #     latitude real
 # )""")
 
-cursor.execute("""INSERT INTO users ("name", "cell_number", "id")
-VALUES ('John Doe', '+1 (608) 123-9823', 1)
-""")
+# cursor.execute("""INSERT INTO users ("name", "cell_number", "id")
+# VALUES ('Jan', '+1 (806) 123-9823', 2)
+# """)
 
 df = pd.read_sql_query("SELECT * FROM users", con)
 print(df.head())
