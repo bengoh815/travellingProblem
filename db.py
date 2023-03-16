5359478a19f99ea23d7fd96cc0b1cae74b700915
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2023-03-16 18:44:42 -0500

    Restructured code
    Hopefully this makes it more efficient as less database connections needs to be called in order to do CRUD operations

--- db.py
+++ db.py
@@ -1,68 +1,56 @@
 import sqlite3
 import pandas as pd
 
-def starter():
-    try:
-        con = sqlite3.connect("./env/mainDB.db")
-        cur = con.cursor()
-        cur.execute("""
-        CREATE TABLE IF NOT EXISTS users(
-            id int,
-            name varchar(255),
-            cell_number varchar(255),
-            address text,
-            latitude real,
-            longtitude real
-        )
-        """)
-        cur.close()
-        print("Succesful initialization of database.")
-
-    except sqlite3.Error as error:
-        print("Error intitalizing db.", error)
-    finally:
-        if con:
-            con.close()
-
-def dbExists():
-    try:
-        con = sqlite3.connect("./env/mainDB.db")
-        cur = con.cursor()
-        listOfTables = cur.execute(
-    """SELECT name FROM sqlite_master WHERE type='table'
-    AND name='users'; """).fetchall()
-        cur.close()
-        return (listOfTables != [])
-        
-    except sqlite3.Error as error:
-        print("Error checking if database exists.", error)
-    finally:
-        if con:
-            con.close()
-
-
-def add_user(id, name, cell_number, address, lat, lon):
-    try:
-        con = sqlite3.connect("mainDB.db")
-        cur = con.cursor()
-
-        sqlite_insert = """INSERT INTO users 
-        (id, name, cell_number, address, latitude, longtitude)
-        VALUES (?, ?, ?, ?, ?, ?)"""
-        data = (id, name, cell_number, address, lat, lon)
-        cur.execute(sqlite_insert, data)
-        con.commit()
-        print("Success data insert into sqlite table.")
-        cur.close()
-    except sqlite3.Error as error:
-        print("Failed to insert data into sqlite table.", error)
-    finally:
-        if con:
-            con.close()
-            print("Connection closed.")
-
-# add_user("Candice", "+1 (723) 938-9827", 9)
 
 # for future playing with two tables
 def add_geo():
     pass
+
+# CONSIDER REFACTORING CODE TO BE MORE MANAGABLE INSTEAD OF 
+# REMAKING CONNECTIONS AND HAVING DUPLICATE CODE
+# def name(con, paras)
+#     do something with con and paras
+
+def connect(path):
+    try :
+        return sqlite3.connect(path)
+    except sqlite3.Error as e:
+        print("Error connecting to db", e)
+        return None
+
+def disconnect(con):
+    con.close()
+    return True
+
+def init(con, tableName, tableCols):
+    query = "CREATE TABLE IF NOT EXISTS {0}({1})".format(tableName, tableCols)
+    cur = con.cursor()
+    cur.execute(query)
+    cur.close()
+    print("Succesful initialization of database.")
+
+def dbExists(con, tableName):
+    query = """
+    SELECT name FROM sqlite_master 
+    WHERE type='table'
+    AND name='{0}'""".format(tableName)
+    cur = con.cursor()
+    listOfTables = cur.execute(query).fetchall()
+    cur.close()
+    return (listOfTables != [])
+
+def add_user(con, tableName, cols, data):
+    try:
+        cur = con.cursor()
+
+        query = """INSERT INTO {0} {1} VALUES {2}""".format(tableName, cols, data)
+        cur.execute(query)
+        con.commit()
+        # print("Success data insert into sqlite table.")
+        cur.close()
+        return True
+    except sqlite3.Error as error:
+        print("Failed to insert data into sqlite table.", error)
+        return False
+
+# add_user("Candice", "+1 (723) 938-9827", 9)
