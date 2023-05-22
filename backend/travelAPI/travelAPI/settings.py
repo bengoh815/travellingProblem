1140d184d796fd33942d60ea6c62ba1068fad171
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2023-05-22 18:27:43 -0500

    Update settings.py
    adding app to project. make sure things are accessible

--- backend/travelAPI/travelAPI/settings.py
+++ backend/travelAPI/travelAPI/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'travelApp.apps.TravelappConfig'
 ]
+
+# TODO: modify when deployment
+CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleWare',
 ]
 
 ROOT_URLCONF = 'travelAPI.urls'
