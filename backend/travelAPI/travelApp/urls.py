4a4e933828a0350e277063e5d6df4cbdd61c4efe
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2023-05-23 15:31:27 -0500

    backend routes finished

--- backend/travelAPI/travelApp/urls.py
+++ backend/travelAPI/travelApp/urls.py
@@ -3,5 +3,8 @@ from travelApp import views
 
 urlpatterns = [
     url(r'^alias$', views.aliasApi),
-    url(r'^alias/([0-9+])$', views.aliasApi)
+    url(r'^alias/([0-9]+)$', views.aliasApi),
+
+    url(r'^users$', views.usersApi),
+    url(r'^users/([0-9]+)$', views.usersApi)
 ]
--- backend/travelAPI/travelApp/views.py
+++ backend/travelAPI/travelApp/views.py
@@ -14,3 +14,51 @@ def aliasApi(req, id=0):
         alias = Alias.objects.all()
         alias_serializer = AliasSerializer(alias, many=True)
         return JsonResponse(alias_serializer.data, safe=False)
+    elif req.method == 'POST':
+        alias_data = JSONParser().parse(req)
+        alias_serializer = AliasSerializer(data=alias_data)
+        if alias_serializer.is_valid():
+            alias_serializer.save()
+            return JsonResponse("Added Successfully", safe=False)
+        return JsonResponse("Failed to Add", safe=False)
+    elif req.method == 'PUT':
+        alias_data = JSONParser().parse(req)
+        alias = Alias.objects.get(AliasId=alias_data['AliasId'])
+
+        alias_serializer = AliasSerializer(alias, data=alias_data)
+        if alias_serializer.is_valid():
+            alias_serializer.save()
+            return JsonResponse("Updated Successfully", safe=False)
+        return JsonResponse("Failed to Update", safe=False)
+    elif req.method == 'DELETE':
+        alias = Alias.objects.get(AliasId=id)
+        alias.delete()
+        return JsonResponse("Deleted Successfully", safe=False)
+    
+
+@csrf_exempt
+def usersApi(req, id=0):
+    if req.method == 'GET':
+        users = Users.objects.all()
+        users_serializer = UsersSerializer(users, many=True)
+        return JsonResponse(users_serializer.data, safe=False)
+    elif req.method == 'POST':
+        users_data = JSONParser().parse(req)
+        users_serializer = UsersSerializer(data=users_data)
+        if users_serializer.is_valid():
+            users_serializer.save()
+            return JsonResponse("Added Successfully", safe=False)
+        return JsonResponse("Failed to Add", safe=False)
+    elif req.method == 'PUT':
+        users_data = JSONParser().parse(req)
+        users = Users.objects.get(UsersId=users_data['UsersId'])
+
+        users_serializer = UsersSerializer(users, data=users_data)
+        if users_serializer.is_valid():
+            users_serializer.save()
+            return JsonResponse("Updated Successfully", safe=False)
+        return JsonResponse("Failed to Update", safe=False)
+    elif req.method == 'DELETE':
+        users = Users.objects.get(UsersId=id)
+        users.delete()
+        return JsonResponse("Deleted Successfully", safe=False)
