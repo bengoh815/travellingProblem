8a64f7c9b4fafff7bb6355fbcbb77f382747b3f9
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2023-05-22 21:29:42 -0500

    finish models
    some weird bug fix

--- backend/travelAPI/travelAPI/settings.py
+++ backend/travelAPI/travelAPI/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleWare',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'travelAPI.urls'
--- backend/travelAPI/travelApp/models.py
+++ backend/travelAPI/travelApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Alias(models.Model):
+    AliasId = models.AutoField(primary_key=True)
+    PhoneNumber = models.CharField(max_length=32, unique=True)
+    Name = models.CharField(max_length=32)
+
+class Users(models.Model):
+    UsersId = models.AutoField(primary_key=True)
+    PhoneNumber = models.CharField(max_length=32, unique=True)
+    Name = models.CharField(max_length=32)
+    Address = models.CharField(max_length=64)
+    Date = models.DateField(auto_now_add=True)
+    Longitude = models.FloatField(max_length=8)
+    Latitude = models.FloatField(max_length=8)
