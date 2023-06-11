e6406f7c9dc1baa0ea8e4f0b3be4c530d3cd6a55
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2023-06-11 01:58:15 -0500

    Stable version of dnd
    Need code review and more extensive testing.
    should be finished

--- frontend/travelui/src/components/Car.js
+++ frontend/travelui/src/components/Car.js
@@ -3,23 +3,23 @@ import styles from "../styles/Car.module.css";
 
 export default function Car(props) {
   const { isOver, setNodeRef } = useDroppable({ id: props.id });
-  const style = { opacity: isOver ? 1 : 0.5 };
+  const style = { opacity: isOver ? 1 : 1 };
+  // change the drop area to glow or something flashy
+  // const style = { background-color: isOver ? rgba(255, 255, 255, .7) : white };
+
   return (
-    <div ref={setNodeRef} style={style} className={styles.container}>
-      <p>{props.driver}'s car</p>
-      <ul>
-        {props.passengers.map((passenger) => (
-          <li key={passenger.Id}>{passenger.Name}</li>
-        ))}
-      </ul>
+    <div className={styles.container}>
+      <div ref={setNodeRef} style={style}>
+        <p>{props.driver}'s car</p>
+        {props.children}
+      </div>
     </div>
   );
 }
-
-/* 
-This styling did not work
-<ul className={style.carList}>
-{props.passengers.map((passenger) => (
-  <li className={style.carListItem} key={passenger.Id}>
-    {passenger.Name}
-  </li> */
+// original idea but due to div 0 height issue
+// <div className={styles.container}>
+// <p>{props.driver}'s car</p>
+// <div ref={setNodeRef} style={style}>
+//   {props.children}
+// </div>
+// </div>
--- frontend/travelui/src/components/CarSelection.js
+++ frontend/travelui/src/components/CarSelection.js
@@ -7,6 +7,7 @@ import styles from "../styles/CarSelection.module.css";
 export default function CarSelection() {
   // Get data from somehow
   const input = {
+    passengers: [],
     unassigned: [
       { Id: 12, Name: "Bradford", Longitude: 6.6, Latitude: 9.7 },
       { Id: 4, Name: "Karina", Longitude: 9.1, Latitude: 9.0 },
@@ -14,16 +15,26 @@ export default function CarSelection() {
       { Id: 5, Name: "Rae", Longitude: 8.1, Latitude: 1.8 },
     ],
     cars: [
-      { Name: "Jen", Passengers: [] },
+      { Id: 1, Name: "Jen", Passengers: [] },
       {
+        Id: 2,
         Name: "Brat",
         Passengers: [
           { Id: 7, Name: "Craig", Longitude: 5.2, Latitude: 6.1 },
           { Id: 3, Name: "Von", Longitude: 5.3, Latitude: 9.5 },
         ],
       },
-      { Name: "Kent", Passengers: [] },
-      { Name: "Alex", Passengers: [] },
+      { Id: 3, Name: "Kent", Passengers: [] },
+      {
+        Id: 4,
+        Name: "Alex",
+        Passengers: [{ Id: 6, Name: "Stu", Longitude: 9.2, Latitude: 7.2 }],
+      },
+    ],
+    assigned: [
+      [7, 2],
+      [3, 2],
+      [6, 4],
     ],
   };
 
@@ -32,44 +43,148 @@ export default function CarSelection() {
 
   function handleDragEnd({ over }) {
     if (over) {
-      // car info
-      const carIndex = over.id;
+      // move obj to destination
+      const desId = over.id;
+      const passengerId = dragObj;
 
-      // passenger info
-      const passengerIndex = dragObj - 1;
-      const passenger = data.unassigned[passengerIndex];
-      const passengerId = passenger.Id;
+      const rtnVal = data.unassigned.findIndex((e) => e.Id === passengerId);
+      if (rtnVal !== -1) {
+        // found in unassigned
+        // get passenger
+        const passenger = data.unassigned[rtnVal];
 
-      // remove passenger from unassigned
-      const newUnassigned = data.unassigned.filter((p) => p.Id !== passengerId);
+        // rmv passenger from unassigned
+        const newUnassigned = data.unassigned.filter(
+          (p) => p.Id !== passengerId
+        );
 
-      // add passenger to car
-      const newPassengers = [...data.cars[carIndex].Passengers, passenger];
-      const newCar = { ...data.cars[carIndex], Passengers: newPassengers };
+        // find car
+        const carIndex = data.cars.findIndex((c) => c.Id === desId);
+        const car = data.cars[carIndex];
 
-      // update cars
-      const newCars = data.cars.map((car, index) => {
-        if (index === carIndex) {
-          return newCar;
-        } else {
-          return car;
+        // put passenger in car
+        const newPassengers = [...car.Passengers, passenger];
+        const newCar = { ...car, Passengers: newPassengers };
+        const newCars = data.cars.map((car, index) => {
+          return index === carIndex ? newCar : car;
+        });
+
+        // update assigned
+        const newAssigned = [...data.assigned, [passengerId, desId]];
+
+        // update data
+        const newData = {
+          ...data,
+          unassigned: newUnassigned,
+          cars: newCars,
+          assigned: newAssigned,
+        };
+        setData(newData);
+      } else {
+        // not found in unassigned
+        // find car
+        const carIdIndex = data.assigned.findIndex((e) => e[0] === passengerId);
+        const carId = data.assigned[carIdIndex][1];
+        const carIndex = data.cars.findIndex((c) => c.Id === carId);
+        const car = data.cars[carIndex];
+
+        // destination car is not itself
+        if (carId !== desId) {
+          // find passenger
+          const passengerIndex = car.Passengers.findIndex(
+            (p) => p.Id === passengerId
+          );
+          const passenger = car.Passengers[passengerIndex];
+
+          // rmv passenger from car
+          const midPassengers = car.Passengers.filter(
+            (p) => p.Id !== passengerId
+          );
+
+          const midCar = { ...car, Passengers: midPassengers };
+
+          const midCars = data.cars.map((car, index) => {
+            return index === carIndex ? midCar : car;
+          });
+
+          // rmv from assigned
+          const midAssigned = data.assigned.filter((e) => e[0] !== passengerId);
+
+          // find car
+          const desCarIndex = data.cars.findIndex((c) => c.Id === desId);
+          const desCar = data.cars[desCarIndex];
+
+          // put passenger in car
+          const newPassengers = [...desCar.Passengers, passenger];
+          const newCar = { ...desCar, Passengers: newPassengers };
+          const newCars = midCars.map((car, index) => {
+            return index === desCarIndex ? newCar : car;
+          });
+
+          // update assigned
+          const newAssigned = [...midAssigned, [passengerId, desId]];
+
+          // update data
+          const newData = {
+            ...data,
+            cars: newCars,
+            assigned: newAssigned,
+          };
+          setData(newData);
         }
-      });
+      }
+    } else {
+      // move passenger to source
+      const passengerId = dragObj;
+      const rtnVal = data.unassigned.findIndex((e) => e.Id === passengerId);
+      if (rtnVal === -1) {
+        // in assigned
+        // find car
+        const carIdIndex = data.assigned.findIndex((e) => e[0] === passengerId);
+        const carId = data.assigned[carIdIndex][1];
+        const carIndex = data.cars.findIndex((c) => c.Id === carId);
+        const car = data.cars[carIndex];
 
-      // update data
-      const newData = { unassigned: newUnassigned, cars: newCars };
-      setData(newData);
+        // find passenger
+        const passengerIndex = car.Passengers.findIndex(
+          (p) => p.Id === passengerId
+        );
+        const passenger = car.Passengers[passengerIndex];
+
+        // rmv passenger from car
+        const newPassengers = car.Passengers.filter(
+          (p) => p.Id !== passengerId
+        );
+
+        const newCar = { ...car, Passengers: newPassengers };
+
+        const newCars = data.cars.map((car, index) => {
+          return index === carIndex ? newCar : car;
+        });
+
+        // this part is wrong but can be saved for over === null
+        // rmv from assigned
+        const newAssigned = data.assigned.filter((e) => e[0] !== passengerId);
+
+        // put passenger in unassigned
+        const newUnassigned = [...data.unassigned, passenger];
+
+        // update data
+        const newData = {
+          ...data,
+          unassigned: newUnassigned,
+          cars: newCars,
+          assigned: newAssigned,
+        };
+        setData(newData);
+      }
     }
     setDragObj(null);
   }
 
-  function movePassenger() {}
-
   function handleDragStart(e) {
     setDragObj(e.active.id);
   }
-
-  // wierd thing where Passenger id cant be 0
 
   return (
     <div className="bg-light">
@@ -80,9 +195,9 @@ export default function CarSelection() {
         <DndContext onDragEnd={handleDragEnd} onDragStart={handleDragStart}>
           <div className={styles.half}>
             <p>People</p>
-            {data.unassigned.map((passenger, index) => (
-              <Passenger key={index} id={index + 1}>
-                {passenger.Name}
+            {data.unassigned.map((p, index) => (
+              <Passenger key={index} id={p.Id}>
+                {p.Name}
               </Passenger>
             ))}
           </div>
@@ -91,10 +206,16 @@ export default function CarSelection() {
             {data.cars.map((car, index) => (
               <Car
                 key={index}
-                id={index}
+                id={car.Id}
                 driver={car.Name}
                 passengers={car.Passengers}
-              />
+              >
+                {car.Passengers.map((p, i) => (
+                  <Passenger key={i} id={p.Id}>
+                    {p.Name}
+                  </Passenger>
+                ))}
+              </Car>
             ))}
           </div>
         </DndContext>
@@ -102,3 +223,5 @@ export default function CarSelection() {
     </div>
   );
 }
+
+// could it be possible instead of rending passengers twice I save them in an array and just use them as I wish?
