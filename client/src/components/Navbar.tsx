be9781b8ba3b31afb521200678cc925db842f966
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2024-02-08 19:34:37 -0600

    Basic home page details
    Need design

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+import { Box } from "@mui/material";
+
+export default function Navbar() {
+  return (
+    <Box display="flex">
+      <Button>Home</Button>
+      <Button>About</Button>
+    </Box>
+  );
+}
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+import Typography from "@mui/material/Typography";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Typography>Family Group Ride Organizer</Typography>
+      <Typography>Come join us now!</Typography>
+    </div>
+  );
+}
