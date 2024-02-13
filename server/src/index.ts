9a6ed5218a81a53c926b7ebd423f07760d7dbe24
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2024-02-13 12:48:54 -0600

    Basic backend structure of project

---
+++ server/src/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello world!"));
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Server running on port ${port}`));
