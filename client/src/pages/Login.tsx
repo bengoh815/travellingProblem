8d5330d01bd931bafe290da6e2d21ff8ed75d097
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2024-02-13 00:11:42 -0600

    Overall Login Implementation
    Need to work on design
    More mobile friendly

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+import { User } from "./Profile";
 import Navbar from "../components/Navbar";
-import { Box } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  Input,
+  InputLabel,
+  Paper,
+  Typography,
+} from "@mui/material";
 
 export default function Login() {
+  const defaultUser: Pick<User, "email"> & {
+    password: string;
+  } = {
+    email: "",
+    password: "",
+  };
+  const [formState, setFormState] = useState(defaultUser);
+  const [error, setError] = useState("");
+
+  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    console.log(name, value);
+    setFormState({ ...formState, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // Send to API
+  };
+
   return (
     <Box>
       <Navbar />
@@ -13,7 +44,56 @@ export default function Login() {
           alignContent: "center",
         }}
       >
-        <Box sx={{ display: "flex", bgcolor: "red" }}></Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <Paper sx={{ p: 3, borderRadius: 5, minWidth: "382px" }}>
+            <Typography variant="h5" sx={{ my: 4, textAlign: "center" }}>
+              Log in
+            </Typography>
+            <form onSubmit={handleSubmit}>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  gap: 3,
+                  m: 2,
+                }}
+              >
+                <FormControl>
+                  <InputLabel htmlFor="su-e">Email</InputLabel>
+                  <Input
+                    id="su-e"
+                    name="email"
+                    type="text"
+                    value={formState.email}
+                    onChange={handleFormChange}
+                  />
+                </FormControl>
+                <FormControl>
+                  <InputLabel htmlFor="su-p">Password</InputLabel>
+                  <Input
+                    id="su-p"
+                    name="password"
+                    type="password"
+                    value={formState.password}
+                    onChange={handleFormChange}
+                  />
+                </FormControl>
+                {error && <Typography color="error">{error}</Typography>}
+
+                <Button variant="contained" type="submit" sx={{ mt: 5 }}>
+                  Log in
+                </Button>
+              </Box>
+            </form>
+          </Paper>
+        </Box>
       </Box>
     </Box>
   );
