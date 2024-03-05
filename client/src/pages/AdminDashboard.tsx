d05bdbbf9cbe6698ace2566841e909c7b90a0f04
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2024-03-05 14:57:47 -0600

    Admin Dashboard table done

--- client/src/pages/AdminDashboard.tsx
+++ client/src/pages/AdminDashboard.tsx
@@ -2,13 +2,14 @@ import { Typography } from "@mui/material";
 
 import Navbar from "../components/Navbar";
 import StyledTable from "../components/Table";
+import AppsTable from "../components/AppsTable";
 
 const AdminDashboard = () => {
   return (
     <div>
       <Navbar />
       <Typography>Driver Application</Typography>
-      <StyledTable />
+      <AppsTable />
 
       <Typography>Organizer Application</Typography>
       <StyledTable />
