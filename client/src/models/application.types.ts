d4c646176398fb0450cf191710d9d05f674cb956
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2024-03-07 22:56:25 -0600

    Refactor types code

---
+++ client/src/models/application.types.ts
@@ -0,0 +1,7 @@
+export type Applications = {
+  firstName: string;
+  lastName: string;
+  appType: string;
+  decision: number;
+  //   0 = pending, 1 = approved, 2 = denied
+};
---
+++ client/src/models/user.types.ts
@@ -0,0 +1,8 @@
+export type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  isAdmin: boolean;
+};
