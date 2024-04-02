d755a834473ca6a44632a3b08ed7518726fb72ff
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2024-04-01 19:46:33 -0500

    structure for user dashboard

--- client/src/components/EventUpcoming.tsx
+++ client/src/components/EventUpcoming.tsx
@@ -1,13 +1,20 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { IEvent } from "../models/event.types";
 
 const EventUpcoming = () => {
   const hasGroup = false;
+  const eventData: IEvent[] = [];
 
   return (
     <>
       <Typography variant="h4">Upcoming Events</Typography>
-      <Typography>Looks like you are not in a group yet</Typography>
-      <Typography>No upcoming events</Typography>
+      {hasGroup ? (
+        <Typography>Looks like you are not in a group yet</Typography>
+      ) : eventData.length === 0 ? (
+        <Typography>No upcoming events</Typography>
+      ) : (
+        eventData.map((e, i) => <Box key={i}>{e.name}</Box>)
+      )}
     </>
   );
 };
