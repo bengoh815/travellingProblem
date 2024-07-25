245ce24bde3d840fb8d28b722d965db398168346
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2024-07-25 18:50:21 -0500

    Current changes to Group architecture

--- server/src/controllers/group.controller.ts
+++ server/src/controllers/group.controller.ts
@@ -3,86 +3,106 @@ import { Status } from "../utils/statusCodes";
 import groupService from "../services/group.service";
 import { handleError } from "../utils/errorHandler";
 
-export const getAllGroups = async (req: Request, res: Response) => {
-  try {
-    const groups = await groupService.getAllGroups();
-    res.status(Status.OK).json(groups);
-  } catch (error: unknown) {
-    handleError(res, "Error fetching groups", error);
-  }
-};
-
-export const createGroup = async (req: Request, res: Response) => {
-  try {
-    const newGroup = await groupService.createGroup(req.body);
-    res.status(Status.Created).json(newGroup);
-  } catch (error: unknown) {
-    handleError(res, "Error creating group", error);
-  }
-};
-
-export const getGroupById = async (req: Request, res: Response) => {
-  try {
-    const group = await groupService.getGroupById(req.params.groupId);
-    if (!group) {
-      return res.status(Status.NotFound).json({ message: "Group not found" });
+export class GroupController {
+  getAllGroups = async (req: Request, res: Response) => {
+    try {
+      const groups = await groupService.getAllGroups();
+      res.status(Status.OK).json(groups);
+    } catch (error: unknown) {
+      handleError(res, "Error fetching groups", error);
     }
-    res.status(Status.OK).json(group);
-  } catch (error: unknown) {
-    handleError(res, "Error getting group", error);
-  }
-};
+  };
 
-export const getGroupUsers = async (req: Request, res: Response) => {
-  try {
-    const users = await groupService.getGroupUsers(req.params.groupId);
-    if (!users) {
-      return res
-        .status(Status.NotFound)
-        .json({ message: "Group users not found" });
+  createGroup = async (req: Request, res: Response) => {
+    try {
+      const newGroup = await groupService.createGroup(req.body);
+      res.status(Status.Created).json(newGroup);
+    } catch (error: unknown) {
+      handleError(res, "Error creating group", error);
     }
-    res.status(Status.OK).json(users);
-  } catch (error: unknown) {
-    handleError(res, "Error getting group users", error);
-  }
-};
-export const getGroupEvents = async (req: Request, res: Response) => {
-  try {
-    const events = await groupService.getGroupEvents(req.params.groupId);
-    if (!events) {
-      return res
-        .status(Status.NotFound)
-        .json({ message: "Group events not found" });
-    }
-    res.status(Status.OK).json(events);
-  } catch (error: unknown) {
-    handleError(res, "Error getting group events", error);
-  }
-};
+  };
 
-export const updateGroup = async (req: Request, res: Response) => {
-  try {
-    const updatedGroup = await groupService.updateGroup(
-      req.params.groupId,
-      req.body
-    );
-    if (!updatedGroup) {
-      return res.status(Status.NotFound).json({ message: "Group not found" });
+  getGroupById = async (req: Request, res: Response) => {
+    try {
+      const group = await groupService.getGroupById(req.params.groupId);
+      if (!group) {
+        return res.status(Status.NotFound).json({ message: "Group not found" });
+      }
+      res.status(Status.OK).json(group);
+    } catch (error: unknown) {
+      handleError(res, "Error getting group", error);
     }
-    res.status(Status.OK).json(updatedGroup);
-  } catch (error: unknown) {
-    handleError(res, "Error updating group", error);
-  }
-};
+  };
 
-export const deleteGroup = async (req: Request, res: Response) => {
-  try {
-    const deletedGroup = await groupService.deleteGroup(req.params.groupId);
-    if (!deletedGroup) {
-      return res.status(Status.NotFound).json({ message: "Group not found" });
+  getGroupUsers = async (req: Request, res: Response) => {
+    try {
+      const users = await groupService.getGroupUsers(req.params.groupId);
+      if (!users) {
+        return res
+          .status(Status.NotFound)
+          .json({ message: "Group users not found" });
+      }
+      res.status(Status.OK).json(users);
+    } catch (error: unknown) {
+      handleError(res, "Error getting group users", error);
     }
-    res.status(Status.NoContent).send();
-  } catch (error: unknown) {
-    handleError(res, "Error deleting group", error);
-  }
-};
+  };
+  getGroupEvents = async (req: Request, res: Response) => {
+    try {
+      const events = await groupService.getGroupEvents(req.params.groupId);
+      if (!events) {
+        return res
+          .status(Status.NotFound)
+          .json({ message: "Group events not found" });
+      }
+      res.status(Status.OK).json(events);
+    } catch (error: unknown) {
+      handleError(res, "Error getting group events", error);
+    }
+  };
+
+  updateGroup = async (req: Request, res: Response) => {
+    try {
+      const updatedGroup = await groupService.updateGroup(
+        req.params.groupId,
+        req.body
+      );
+      if (!updatedGroup) {
+        return res.status(Status.NotFound).json({ message: "Group not found" });
+      }
+      res.status(Status.OK).json(updatedGroup);
+    } catch (error: unknown) {
+      handleError(res, "Error updating group", error);
+    }
+  };
+
+  deleteGroup = async (req: Request, res: Response) => {
+    try {
+      const deletedGroup = await groupService.deleteGroup(req.params.groupId);
+      if (!deletedGroup) {
+        return res.status(Status.NotFound).json({ message: "Group not found" });
+      }
+      res.status(Status.NoContent).send();
+    } catch (error: unknown) {
+      handleError(res, "Error deleting group", error);
+    }
+  };
+
+  addUserToGroup = async (req: Request, res: Response) => {
+    try {
+      // Do something
+    } catch (error: unknown) {
+      handleError(res, "Error adding user to group", error);
+    }
+  };
+
+  delUserFromGroup = async (req: Request, res: Response) => {
+    try {
+      // Do something
+    } catch (error: unknown) {
+      handleError(res, "Error deleting user from group", error);
+    }
+  };
+}
+
+export default new GroupController();
--- server/src/routes/group.routes.ts
+++ server/src/routes/group.routes.ts
@@ -1,13 +1,5 @@
 import express from "express";
-import {
-  getAllGroups,
-  createGroup,
-  getGroupById,
-  getGroupUsers,
-  getGroupEvents,
-  updateGroup,
-  deleteGroup,
-} from "../controllers/group.controller";
+import GroupController from "../controllers/group.controller";
 import { APIVersion } from "../utils/apiVersion";
 
 const groupsRouter = express.Router();
@@ -16,12 +8,23 @@ const groupsRouter = express.Router();
 const v1 = APIVersion.v1;
 
 // Routes
-groupsRouter.get(`${v1}/groups`, getAllGroups);
-groupsRouter.post(`${v1}/groups`, createGroup);
-groupsRouter.get(`${v1}/groups/:groupId`, getGroupById);
-groupsRouter.get(`${v1}/groups/:groupId/users`, getGroupUsers);
-groupsRouter.get(`${v1}/groups/:groupId/events`, getGroupEvents);
-groupsRouter.put(`${v1}/groups/:groupId`, updateGroup);
-groupsRouter.delete(`${v1}/groups/:groupId`, deleteGroup);
+groupsRouter.get(`${v1}/groups`, GroupController.getAllGroups);
+groupsRouter.post(`${v1}/groups`, GroupController.createGroup);
+groupsRouter.get(`${v1}/groups/:groupId`, GroupController.getGroupById);
+groupsRouter.get(
+  `${v1}/groups/:groupId/events`,
+  GroupController.getGroupEvents
+);
+groupsRouter.put(`${v1}/groups/:groupId`, GroupController.updateGroup);
+groupsRouter.delete(`${v1}/groups/:groupId`, GroupController.deleteGroup);
+groupsRouter.get(`${v1}/groups/:groupId/users`, GroupController.getGroupUsers);
+groupsRouter.post(
+  `${v1}/groups/:groupId/users`,
+  GroupController.addUserToGroup
+);
+groupsRouter.delete(
+  `${v1}/groups/:groupId/users`,
+  GroupController.delUserFromGroup
+);
 
 export default groupsRouter;
